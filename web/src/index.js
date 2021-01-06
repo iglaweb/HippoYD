ac35b2a6e68c9681ddf126cd40cad4a169ff0b58
Igor Lashkov <igor-lashkov@ya.ru>
2021-01-07 00:15:14 +0300

    Add camera page;

--- web/src/index.js
+++ web/src/index.js
@@ -2,6 +2,7 @@ let express = require("express");
 let app = express();
 let port = 8080;
 
+app.use('/camera', express.static("./src/opencv"));
 app.use(express.static("./src"));
 
 app.listen(port, function () {
--- web/src/predict.js
+++ web/src/predict.js
@@ -1,7 +1,7 @@
 if(typeof jQuery!=='undefined'){
     console.log('jQuery Loaded');
 }
-else{
+else {
     console.log('jQuery not loaded yet');
 }
 
@@ -17,34 +17,60 @@ $("#image-selector").change(function () {
 	reader.readAsDataURL(file);
 });
 
-
-let model;
-$(document).ready(async function () {
+async function init_models (is_camera) {
+	cur_page = window.location.href
+	console.log(cur_page);
+	var is_camera = cur_page.indexOf("/camera") > -1;
+	console.log(is_camera);
 	$('.progress-bar').show();
 	try {
-		model = await tf.loadLayersModel('tfjs_model_40/model.json');
+		model = await tf.loadLayersModel(is_camera ? '../tfjs_model_40/model.json' : 'tfjs_model_40/model.json');
 	} catch(e) {
 		console.log("the model could not be loaded")
 		console.log(e)
 	}
 	console.log("the model loaded successfully")
 	$('.progress-bar').hide();
-});
+}
 
-$("#predictBtn").click(async function () {
-	let image = $('#selected-image').get(0);
-	
+
+let model;
+$(document).ready(init_models(true));
+
+predict_image = async function (image) {
 	let pre_image = tf.browser.fromPixels(image, 1)
 		.resizeNearestNeighbor([100, 100])
 		.expandDims()
 		.toFloat()
 		.div(255.0)
-		.reverse(-1); 
-		console.log(pre_image);
+		.reverse(-1);
+	console.log(pre_image);
 	let predict_result = await model.predict(pre_image).data();
 	let probability = predict_result[0];
-	console.log(probability);
+	return probability;
+}
 
+
+function predict_image_sync(image) {
+	console.log('Resolve image');
+	console.log(image);
+	let pre_image = tf.browser.fromPixels(image, 1)
+		.resizeNearestNeighbor([100, 100])
+		.expandDims()
+		.toFloat()
+		.div(255.0)
+		.reverse(-1);
+	console.log('Image prepared');
+	console.log(pre_image);
+	let predict_result = model.predict(pre_image).data();
+	let probability = predict_result[0];
+	return probability;
+}
+
+$("#predictBtn").click(async function () {
+	let image = $('#selected-image').get(0);
+	let probability = await predict_image(image);
+	console.log(probability);
 	$("#prediction-list").empty();
 	$("#prediction-list").append(`<li>probability: ${parseInt(Math.trunc(probability * 100))} %</li>`);
 });
